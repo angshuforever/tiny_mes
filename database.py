520392b816bc1efe877f0e06154996a7ebf7864c
Angshuman Nandi <144582719+angshuforever@users.noreply.github.com>
2025-01-12 20:26:46 +0530

    Removing personal creds

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-POSTGRES_URL = "postgresql://angshu:angshu@localhost:5433/angdb"
+#POSTGRES_URL = "postgresql://<username>:<password>@<postgress_host_ip>:<port>/<db>"
+POSTGRES_URL = "postgresql://<my_user_name>:<password_removed>@localhost:<port_removed>/<db_name_removed>"
 
 engine = create_engine(POSTGRES_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
